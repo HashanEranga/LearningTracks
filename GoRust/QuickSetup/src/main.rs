4158ed7ebf1caf2d4f72bd3449ea43a568755e50
HashanEranga <ccmrhe@gmail.com>
2026-06-09 23:48:57 +0530

    gorust | rust | compound types

---
+++ GoRust/QuickSetup/src/main.rs
@@ -0,0 +1,6 @@
+mod compound_types;
+
+fn main() {
+    println!("Hello, world!");
+    compound_types::compound_types();
+}
---
+++ GoRust/QuickSetup/src/variables.rs
@@ -0,0 +1,5 @@
+pub(crate) fn variables(){
+    println!("Variables and constants");
+    let x = 10;
+    
+}
