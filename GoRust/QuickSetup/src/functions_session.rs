f5d8eff133429425951c85764dfb23e431ca295f
HashanEranga <ccmrhe@gmail.com>
2026-06-10 00:35:39 +0530

    gorust | rust | functions

---
+++ GoRust/QuickSetup/src/functions_session.rs
@@ -0,0 +1,17 @@
+pub(crate) fn functions_session(s: &str){
+    println!("{s}");
+}
+
+pub(crate) fn multiplication(num1: i32, num2: i32) -> i32 {
+    println!("calculating multiplication of num1 {num1} and num2 {num2}");
+    num1 * num2
+}
+
+pub(crate) fn basic_math(num1: i32, num2:i32) -> (i32, i32, i32){
+    (num1 + num2, num2 - num1, num1 * num2)
+}
+
+// expression vs statement 
+// statement always returns a unit value
+// expression returns a valid value including a unit value
+
--- GoRust/QuickSetup/src/main.rs
+++ GoRust/QuickSetup/src/main.rs
@@ -1,6 +1,28 @@
+use crate::functions_session::basic_math;
+
 mod compound_types;
+mod functions_session;
 
 fn main() {
     println!("Hello, world!");
     compound_types::compound_types();
+    
+    functions_session::functions_session("hello world");
+    let sample_string = "this is a sample string";
+    functions_session::functions_session(sample_string);
+    
+    let answer = functions_session::multiplication(12,12);
+    println!("the answer is {}", answer);
+    
+    let (answer_add, answer_sub, answer_mul) = basic_math(12, 12);
+    println!("the answer_add {}, answer_sub {}, answer_mul {}", answer_add, answer_sub, answer_mul);
+
+    // code blocks
+    let full_name = {
+        let first_name = "hashan";
+        let last_name = "eranga";
+        format!("{first_name} {last_name}")
+    };
+
+    println!("{full_name}");
 }
